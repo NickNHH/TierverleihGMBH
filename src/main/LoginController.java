bc08e02ea29f1622d2ac49ddb25e9389506011e8
nico <nico.tuscano@adnovum.ch>
2019-05-29 12:41:48 +0200

    Add files and rough structure for login page

---
+++ src/main/LoginController.java
@@ -0,0 +1,9 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public Button backBtn;
+    public TextField passwordTf;
+}
