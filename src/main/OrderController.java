a7a188ac41ed9c647a2f5138b4046d68a0d6f795
nico <nico.tuscano@adnovum.ch>
2019-06-03 15:52:58 +0200

    Add fxml and controller for order site

---
+++ src/main/OrderController.java
@@ -0,0 +1,15 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class OrderController {
+    public Button backBtn;
+    public TextField lastNameTf;
+    public TextField firstNameTf;
+    public TextField emailTf;
+    public TextField streetTf;
+    public TextField zipCodeTf;
+    public TextField reasonTf;
+    public Button orderBtn;
+}
