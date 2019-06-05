ee72d53a52bf43b9d50ec12779e4e7367f811f3c
nico <nico.tuscano@adnovum.ch>
2019-06-05 17:28:34 +0200

    Add files and structure for worker overview of animals site

---
+++ src/main/AnimalOverviewController.java
@@ -0,0 +1,10 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class AnimalOverviewController {
+    public Pane pane;
+    public Button backBtn;
+    public Button newAnimalBtn;
+}
