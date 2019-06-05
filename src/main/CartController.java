3344e3d249b459b3b5766d63876d19d5852d9b62
nico <nico.tuscano@adnovum.ch>
2019-06-05 17:22:03 +0200

    Add files and structure for shopping car site

---
+++ src/main/CartController.java
@@ -0,0 +1,10 @@
+package main;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class CartController {
+    public Pane pane;
+    public Button backBtn;
+    public Button orderBtn;
+}
