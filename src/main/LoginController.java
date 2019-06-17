5a3653c76fb88ec5d68585251aeab527a54fa397
5ia17jowaeger <5ia17jowaeger@gibwintra.ch>
2019-06-17 14:00:38 +0200

    add the login feature

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -1,6 +1,8 @@
 package main;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -22,4 +24,17 @@ public class LoginController {
         logic.switchScene("fxml/MainFxml.fxml", backBtn, fxmlLoader);
         ((MainController) fxmlLoader.getController()).init();
     }
+    public void enter() throws IOException{
+    if(passwordTf.getText().equals("Tierequälen")) {
+    	logic.switchScene("fxml/AnimalOverviewFxml.fxml",  null,fxmlLoader);
+    }else {
+    	Alert alert = new Alert(AlertType.WARNING);
+    	alert.setTitle("Incorrect Password");
+    	alert.setHeaderText("Are you really an Employee?");
+    	alert.setContentText("Please try again! And if you fail again we will come to you and burn down your home!");
+
+    	alert.showAndWait();
+    }
+    
+}
 }
