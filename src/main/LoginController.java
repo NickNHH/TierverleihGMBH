5ad0c6736ff8198c0b0e7f6ea69ad0d70d061896
5ia17jowaeger <5ia17jowaeger@gibwintra.ch>
2019-06-24 11:34:04 +0200

    finished login feature

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -1,40 +1,60 @@
 package main;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
-    public Button backBtn;
-    public TextField passwordTf;
+	public Button backBtn;
+	public PasswordField passwordTf;
+	public Pane pane;
 
-    private Logic logic = new Logic();
-    private FXMLLoader fxmlLoader = new FXMLLoader();
+	private Logic logic = new Logic();
+	private FXMLLoader fxmlLoader = new FXMLLoader();
 
-    void init() {
-        //Loads class
-    }
+	void init() {
+		// Loads class
+	}
 
-    //Opens main window and closes this window.
-    public void goBack() throws IOException {
-        logic.switchScene("fxml/MainFxml.fxml", backBtn, fxmlLoader);
-        ((MainController) fxmlLoader.getController()).init();
-    }
-    public void enter() throws IOException{
-    if(passwordTf.getText().equals("Tierequälen")) {
-    	logic.switchScene("fxml/AnimalOverviewFxml.fxml",  null,fxmlLoader);
-    }else {
-    	Alert alert = new Alert(AlertType.WARNING);
-    	alert.setTitle("Incorrect Password");
-    	alert.setHeaderText("Are you really an Employee?");
-    	alert.setContentText("Please try again! And if you fail again we will come to you and burn down your home!");
+	// Opens main window and closes this window.
+	public void goBack() throws IOException {
+		logic.switchScene("fxml/MainFxml.fxml", backBtn, fxmlLoader);
+		((MainController) fxmlLoader.getController()).init();
+	}
 
-    	alert.showAndWait();
-    }
-    
-}
+	public void enter() throws IOException {
+		pane.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				if (passwordTf.getText().equals("tierequälen")) {
+					try {
+						logic.switchScene("fxml/AnimalOverviewFxml.fxml", null, fxmlLoader);
+						  Stage stage = (Stage) pane.getScene().getWindow();
+					        stage.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Incorrect Password");
+					alert.setHeaderText("Are you really an Employee?");
+					alert.setContentText(
+							"Please try again! And if you fail again we will come to you and burn down your home!");
+
+					alert.showAndWait();
+				}
+			}
+		});
+	}
+
 }
