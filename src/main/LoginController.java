38ad17e05edcf1d73838a7158f137e0e2315309b
nico <nico.tuscano@adnovum.ch>
2019-06-24 11:39:57 +0200

    Fix imports and add lambda

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -1,12 +1,10 @@
 package main;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -30,31 +28,26 @@ public class LoginController {
 		((MainController) fxmlLoader.getController()).init();
 	}
 
-	public void enter() throws IOException {
-		pane.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				// TODO Auto-generated method stub
-				if (passwordTf.getText().equals("tierequ‰len")) {
-					try {
-						logic.switchScene("fxml/AnimalOverviewFxml.fxml", null, fxmlLoader);
-						  Stage stage = (Stage) pane.getScene().getWindow();
-					        stage.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} else {
-					Alert alert = new Alert(AlertType.WARNING);
-					alert.setTitle("Incorrect Password");
-					alert.setHeaderText("Are you really an Employee?");
-					alert.setContentText(
-							"Please try again! And if you fail again we will come to you and burn down your home!");
+	public void enter() {
+		pane.setOnKeyPressed(event -> {
+            if (passwordTf.getText().equals("tierequ√§len")) {
+                try {
+                    logic.switchScene("fxml/AnimalOverviewFxml.fxml", null, fxmlLoader);
+                      Stage stage = (Stage) pane.getScene().getWindow();
+                        stage.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Incorrect Password");
+                alert.setHeaderText("Are you really an Employee?");
+                alert.setContentText(
+                        "Please try again! And if you fail again we will come to you and burn down your home!");
 
-					alert.showAndWait();
-				}
-			}
-		});
+                alert.showAndWait();
+            }
+        });
 	}
 
 }
